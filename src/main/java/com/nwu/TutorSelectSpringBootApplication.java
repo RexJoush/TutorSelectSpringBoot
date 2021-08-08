e2aab13dba8c4ce25bc3b298c36f959f27e4ad2f
Joush <37282701+RexJoush@users.noreply.github.com>
2021-08-08 17:18:43 +0800

    2021.08.08 Init

---
+++ src/main/java/com/nwu/TutorSelectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSelectSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorSelectSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nwu/TutorSelectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorSelectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
