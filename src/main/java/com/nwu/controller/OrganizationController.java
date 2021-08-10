1bf1ca5601c590686396fd0bd1af349fe9a9af74
dynamic <1115718363@qq.com>
2021-08-10 10:00:37 +0800

    '系统管理---院系初始化'

---
+++ src/main/java/com/nwu/controller/OrganizationController.java
@@ -0,0 +1,21 @@
+package com.nwu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author dynamic
+ * @since 2021-08-10
+ */
+@RestController
+@RequestMapping("/organization")
+public class OrganizationController {
+
+}
+
---
+++ src/main/java/com/nwu/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.nwu.service.impl;
+
+import com.nwu.entity.Organization;
+import com.nwu.mapper.OrganizationMapper;
+import com.nwu.service.OrganizationService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author dynamic
+ * @since 2021-08-10
+ */
+@Service
+public class OrganizationServiceImpl extends ServiceImpl<OrganizationMapper, Organization> implements OrganizationService {
+
+}
