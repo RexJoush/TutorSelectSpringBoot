b1a3fa43d41239d9ec3c68d15d3d015923315b5c
Joush <37282701+RexJoush@users.noreply.github.com>
2021-08-13 15:26:45 +0800

    2021.08.13 Init template by R.Joush

--- src/main/java/com/nwu/service/impl/QueryDepartmentSecretaryServiceImpl.java
+++ src/main/java/com/nwu/service/impl/QueryDepartmentSecretaryServiceImpl.java
@@ -1,19 +1,14 @@
 package com.nwu.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.nwu.entity.ApplyType;
 import com.nwu.entity.query.QueryDepartmentSecretaryInit;
-import com.nwu.mapper.ApplyTypeMapper;
 import com.nwu.mapper.QueryDepartmentSecretaryMapper;
 import com.nwu.service.QueryDepartmentSecretaryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 public class QueryDepartmentSecretaryServiceImpl extends ServiceImpl<QueryDepartmentSecretaryMapper, QueryDepartmentSecretaryInit> implements QueryDepartmentSecretaryService {
