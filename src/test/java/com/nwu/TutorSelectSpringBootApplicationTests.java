b1a3c3e33553b78c27470479cb1c33e260ac83d4
dynamic <1115718363@qq.com>
2021-08-16 11:40:51 +0800

    删除

--- src/test/java/com/nwu/TutorSelectSpringBootApplicationTests.java
+++ src/test/java/com/nwu/TutorSelectSpringBootApplicationTests.java
@@ -1,8 +1,6 @@
 package com.nwu;
 
-import com.nwu.mapper.QueryDepartmentSecretaryMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
