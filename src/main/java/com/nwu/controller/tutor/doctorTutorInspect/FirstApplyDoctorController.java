93876f01adfce0b5df42c1e20ab4c1d50c4ee810
xiao-jin-gang <71590074+xiao-jin-gang@users.noreply.github.com>
2021-08-25 11:08:26 +0800

    博士导师第二页更新zjz

--- src/main/java/com/nwu/controller/tutor/doctorTutorInspect/FirstApplyDoctorController.java
+++ src/main/java/com/nwu/controller/tutor/doctorTutorInspect/FirstApplyDoctorController.java
@@ -1,9 +1,11 @@
 package com.nwu.controller.tutor.doctorTutorInspect;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nwu.entities.Apply;
 import com.nwu.entities.Organization;
 import com.nwu.entities.tutor.FirstPage;
+import com.nwu.entities.tutor.SecondPage;
 import com.nwu.results.Result;
 
 import com.nwu.results.ResultCode;
@@ -38,13 +40,15 @@ public class FirstApplyDoctorController {
     // 导师申请表
     @Resource
     private TutorInspectService tutorInspectService;
+
     @Resource
     private OrganizationService organizationService;
 
-    public static String tutorId="20133220";
+    public final String tutorId="20133220";
+
     @ApiOperation("保存博士基本信息和申请类别表")
     @PostMapping("/saveBaseInfo/{applyId}/{applyCondition}")
-    public Result SaveOrUpdateApplyDoctor(@RequestBody FirstPage firstPage, @PathVariable("applyId") Integer applyId, @PathVariable("applyCondition") Integer applyCondition) {
+    public Result SaveOrUpdateApplyDoctor(@RequestBody FirstPage firstPage, @PathVariable("applyId") int applyId, @PathVariable("applyCondition") int applyCondition) {
 
         // 没有申请过和正在申请中都进来 根据applyCondition判断是插入还是修改apply
         if (applyCondition == 102) {
@@ -73,19 +77,41 @@ public class FirstApplyDoctorController {
 
         } else if (applyCondition == 101) {
             // 已经申请过此岗位，但信息未填写完成，第一页不修改，继续第二页，直接返回
-            return new Result(ResultCode.SUCCESS);
-            // 2.根据apply表去修改教师申请表
-            // 根据tutor_id和status查出与apply对应的tutor_inspect的id
-            // int id = mainBoardService.getApplyIdByTutorIdAndStatus(tutorId, 0);
-            //System.out.println(tutorInspect);
-//          tutorInspectService.updateTutorInspect(tutorInspect);
-
-
+            //根据填写信息查询出对应的主键id
+            int id = mainBoardService.getId(firstPage.getNumber(), 1, 0);
+            System.out.println(id);
+            return new Result(ResultCode.SUCCESS,id);
         }
         return Result.FAIL();
 
 
     }
 
+
+    @ApiOperation("更新第二页博士基本信息")
+    @PostMapping("updateSecondPage/{applyId}/{id}")
+    public Result updateSecondPage(@RequestBody SecondPage secondPage,@PathVariable("applyId") Integer applyId,@PathVariable("id") Integer id){
+        //根据主键更新第二页信息
+        if (!"".equals(id) && !"".equals(secondPage.getApplySubject()))
+        {
+            //设置json传送到数据库中   设置专家称号的字符串
+            secondPage.setExpertTitlesJson(JSON.toJSONString(secondPage.getExpertTitles()));
+            // 设置学术团体、任何种职务，有何社会兼职的字符串
+            secondPage.setGroupsOrPartTimeJobsJson(JSON.toJSONString(secondPage.getGroupsOrPartTimeJobsJson()));
+            // 分别设置一级学科代码和名称  doctoralMasterSubjectCodeName=0818 地质资源与地质工程,
+            secondPage.setDoctoralMasterSubjectCode(secondPage.getDoctoralMasterSubjectCodeName().split(" ")[0]);
+            //名称
+            secondPage.setDoctoralMasterSubjectName(secondPage.getDoctoralMasterSubjectCodeName().split("")[1]);
+            //更新学科信息
+            mainBoardService.updateApplySubject(id, Integer.parseInt(secondPage.getApplySubject()));
+            // 更新第二页信息
+            tutorInspectService.updateTutorInspectSecond(id, secondPage);
+            //返回成功信息
+            return new Result(ResultCode.SUCCESS);
+
+        }
+        //失败
+        return Result.FAIL();
+    }
 
 }
