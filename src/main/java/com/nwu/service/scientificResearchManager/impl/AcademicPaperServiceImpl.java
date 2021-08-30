8863e5b4db90f5af8ac6d5a4d7d1f5064e9ec8c5
Anna667 <632177309@qq.com>
2021-08-30 22:08:35 +0800

    Anna 2021-08-30 22:08

--- src/main/java/com/nwu/service/scientificResearchManager/impl/AcademicPaperServiceImpl.java
+++ src/main/java/com/nwu/service/scientificResearchManager/impl/AcademicPaperServiceImpl.java
@@ -6,6 +6,8 @@ import com.nwu.service.scientificResearchManager.AcademicPaperService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 /**
  * <p>
  *  服务实现类
