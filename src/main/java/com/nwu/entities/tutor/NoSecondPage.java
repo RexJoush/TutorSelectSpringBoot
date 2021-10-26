131ec493cc5178bf7e46805f0b9f3090f8c8e506
Joush <37282701+RexJoush@users.noreply.github.com>
2021-10-26 10:32:11 +0800

    2021.10.26 添加专硕免审字段

--- src/main/java/com/nwu/entities/tutor/NoSecondPage.java
+++ src/main/java/com/nwu/entities/tutor/NoSecondPage.java
@@ -14,56 +14,70 @@ import java.util.List;
 @AllArgsConstructor
 public class NoSecondPage {
 
-    //免审
+    // 免审
 
     // 申请 id
     private int applyId;
 
-    //主要研究方向
+    // 主要研究方向
     private String researchDirections;
 
-    //申请学科负责单位
+    // 申请学科负责单位
     private String appliedSubjectUnit;
 
-    //一级学科代码
+    // 一级学科代码
     private String appliedSubjectCode;
 
-    //一级学科名称
+    // 一级学科名称
     private String appliedSubjectName;
 
-    //一级学科代码及名称 辅助字段
+    // 申请类别负责单位
+    private String professionalAppliedSubjectUnit;
+
+    // 类别代码
+    private String professionalAppliedSubjectCode;
+
+    // 类别名称
+    private String professionalAppliedSubjectName;
+
+    // 领域代码
+    private String professionalFieldCode;
+
+    // 领域名称
+    private String professionalFieldName;
+
+    // 一级学科代码及名称 辅助字段
     private String doctoralMasterSubjectCodeName;
 
-    //免审条件
+    // 免审条件
     private String exemptionConditions;
 
-    //免审条件路径
+    // 免审条件路径
     private String exemptionConditionsMaterials;
 
-    //申请类别 文史 理工 交叉
+    // 申请类别 文史 理工 交叉
     private Integer applySubject;
 
     /* 科研教学情况 */
 
-    //科研项目 存入数据库
+    // 科研项目 存入数据库
     private String researchProjectsJson;
 
-    //科研项目 辅助字段
+    // 科研项目 辅助字段
     private List<ResearchProject> researchProjects;
 
-    //科研教学情况 存入数据库
+    // 科研教学情况 存入数据库
     private String teachingAwardsJson;
 
-    //科研教学情况 辅助字段
+    // 科研教学情况 辅助字段
     private List<TeachingAward> teachingAwards;
 
     /* 博导免审 */
 
-    //硕导上岗时间
+    // 硕导上岗时间
     private String masterDirectorTime;
-    //硕士招生学科
+    // 硕士招生学科
     private String masterDirectorSubject;
-
 
 
 }
